02eadc482bf8526683cefcd35656576a02ad84b0
Kyle Mathews <mathews.kyle@gmail.com>
2016-03-13 14:24:42 -0700

    Fix condition

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ class BlogIndex extends React.Component {
       access(page, 'data.date')
     ).reverse()
     sortedPages.forEach((page) => {
-      if (access(page, 'file.ext') === 'md' && include(page.path, '/404/')) {
+      if (access(page, 'file.ext') === 'md' && !include(page.path, '/404')) {
         const title = access(page, 'data.title') || page.path
         pageLinks.push(
           <li
