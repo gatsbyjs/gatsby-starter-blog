f7fed5a3ce0f8cb57b23465e0f4c09bf851d9850
Kyle Mathews <mathews.kyle@gmail.com>
2016-08-02 15:40:43 -0600

    No reason to set maximum-scale unless building web app

--- html.js
+++ html.js
@@ -27,7 +27,7 @@ module.exports = React.createClass({
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0 maximum-scale=5.0"
+            content="width=device-width, initial-scale=1.0"
           />
           <title>{title}</title>
           <TypographyStyle typography={typography} />
