0f25ed1579b39a102eed7db78b3d7a4e2bd994ed
Karol Musur <kamusur@gmail.com>
2017-10-21 22:54:44 +0200

    Remove underline on hyperlinked images

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,12 @@
 import Typography from 'typography'
 import Wordpress2016 from 'typography-theme-wordpress-2016'
 
+Wordpress2016.overrideThemeStyles = () => ({
+  'a.gatsby-resp-image-link': {
+    boxShadow: 'none',
+  }
+})
+
 const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
