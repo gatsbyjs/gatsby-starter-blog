315939b93c5bd768a89a4bcd9d4fe15a3658fa49
Nicholas Young <nyoung@uptime.ventures>
2017-11-12 13:25:31 -0700

    fix(rss): generate slug for rss

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const _ = require("lodash")
 const Promise = require('bluebird')
 const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators
@@ -41,4 +42,17 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
       })
     )
   })
+}
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
 }
