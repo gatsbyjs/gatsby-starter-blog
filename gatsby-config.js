696b75cb1feab9d6e18951741f09ad9c91b96481
Kyle Mathews <mathews.kyle@gmail.com>
2017-11-13 10:20:32 -0800

    Add pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter Blog",
-    author: "Kyle Mathews",
-    description: "A starter blog demonstrating what Gatsby can do.",
-    siteUrl: "https://gatsbyjs.github.io/gatsby-starter-blog/",
+    title: 'Gatsby Starter Blog',
+    author: 'Kyle Mathews',
+    description: 'A starter blog demonstrating what Gatsby can do.',
+    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
+  pathPrefix: '/gatsby-starter-blog',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/pages`,
-        name: "pages",
+        name: 'pages',
       },
     },
     {
@@ -29,9 +30,9 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants",
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
         ],
       },
     },
@@ -47,9 +48,9 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: "gatsby-plugin-typography",
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: "src/utils/typography",
+        pathToConfigModule: 'src/utils/typography',
       },
     },
   ],
