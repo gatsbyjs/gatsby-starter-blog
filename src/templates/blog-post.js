aa04a1713fa919dc3cef171505c54500327cdd12
mottox2 <y.mottox2@gmail.com>
2018-06-18 20:42:40 +0900

    Import Link from Gatsby

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
+import { Link } from 'gatsby'
 import get from 'lodash/get'
 
 import Bio from '../components/Bio'
