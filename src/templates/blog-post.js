a31518dc3df671930432673d3cc355f8570efbe8
Matt <mattbag00@gmail.com>
2018-08-22 15:37:41 +1000

    Import graphql from gatsby (#119)
    Removes warning message for deprecation

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { Link } from 'gatsby'
+import { Link,graphql } from 'gatsby'
 import get from 'lodash/get'
 
 import Bio from '../components/Bio'
