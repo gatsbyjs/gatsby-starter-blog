3cf383d58de209a583121c456fcceda308ad6e57
pixelyunicorn <git@melody.cool>
2018-11-06 01:53:49 -0500

    Give 404 page a proper layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <Layout location={this.props.location}>
+        <h1>Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    )
+  }
+}
 
 export default NotFoundPage
