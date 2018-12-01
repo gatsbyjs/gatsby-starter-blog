947ce18a2a2033da54df3f0a4e604d0dcaa79769
Kyle Mathews <mathews.kyle@gmail.com>
2018-11-30 23:45:10 -0800

    Update demo url

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Gatsby Starter Blog',
     author: 'Kyle Mathews',
     description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    siteUrl: 'https://gatsby-starter-blog-demo.netlify.com/',
   },
   pathPrefix: '/gatsby-starter-blog',
   plugins: [
