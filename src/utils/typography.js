1312c8ecd59d6771caa76db34a4be2b179dd3d1d
wangcch <ci713@qq.com>
2019-03-10 03:30:41 +0800

    use typography-theme-github

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,7 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import githubTheme from "typography-theme-github"
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(githubTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
