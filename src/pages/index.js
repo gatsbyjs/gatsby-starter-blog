3a3fb95ee2179eb00ed6ca1560891a39a9ee4ccf
Angelica Gonzalez <agonzalez0515@gmail.com>
2019-03-30 20:58:28 -0700

    Remove layout stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,20 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import SEO from '../components/seo'
 
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
-        <Bio />
+      <React.Fragment>
+      <SEO title='Angie Gonzalez' />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
             <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
+              <h3>
                 <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
                   {title}
                 </Link>
@@ -41,7 +28,7 @@ class BlogIndex extends React.Component {
             </div>
           )
         })}
-      </Layout>
+      </React.Fragment>
     )
   }
 }
@@ -50,11 +37,6 @@ export default BlogIndex
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
