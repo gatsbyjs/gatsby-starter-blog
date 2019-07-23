ae1bb12fcfa2e9f77fd35d162c49b157e27b8dad
ENvironmentSet <sbshw1@gmail.com>
2019-07-23 22:27:48 +0900

    remove prop-types

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
@@ -76,13 +75,6 @@ SEO.defaultProps = {
   lang: 'en',
   meta: [],
   description: '',
-};
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 };
 
 export default SEO;
