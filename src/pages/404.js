8a0ccdb6cc8e1527ed2e55e8a0f15eadcefb4580
ENvironmentSet <sbshw1@gmail.com>
2019-07-25 02:36:44 +0900

    remake 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 
-import Layout from 'components/layout';
 import SEO from 'components/seo';
+import BaseLayout from 'components/templates/BaseLayout';
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
-
+export default function NotFoundPage() {
+  function NotFoundMessage() {
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title='404: Not Found' />
+      <>
         <h1>Not Found</h1>
-        <p>You just hit a route that doesn't exist... the sadness.</p>
-      </Layout>
+        <p>
+          Oops, My router function has produced
+          {' '}
+          <strong>
+            <a href='https://wiki.haskell.org/Bottom'>⊥</a>
+          </strong>,
+          You should check your URI parameters.
+        </p>
+      </>
     );
   }
-}
 
-export default NotFoundPage;
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`;
+  return (
+    <BaseLayout>
+      <SEO title='404: Not Found' />
+      <NotFoundMessage />
+    </BaseLayout>
+  );
+};
