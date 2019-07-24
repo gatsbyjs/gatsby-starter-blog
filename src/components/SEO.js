8a17200abaf8fd5ff64461e5c96720d001a9e688
ENvironmentSet <sbshw1@gmail.com>
2019-07-25 01:54:36 +0900

    remake SEO

---
+++ src/components/SEO.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import Helmet from 'react-helmet';
+
+import useSiteMetadata from 'utils/useSiteMetadata';
+
+export default function SEO({ description, lang = 'en', meta = [], title }) {
+  const { description: siteDescription, author } = useSiteMetadata();
+  const metaDescription = description || siteDescription;
+
+  return (
+    <Helmet
+      htmlAttributes={
+        {
+        lang,
+        }
+      }
+      title={title}
+      titleTemplate={`%s | ${title}`}
+      meta={
+        [
+          {
+            name: 'description',
+            content: metaDescription,
+          },
+          {
+            property: 'og:title',
+            content: title,
+          },
+          {
+            property: 'og:description',
+            content: metaDescription,
+          },
+          {
+            property: 'og:type',
+            content: 'website',
+          },
+          {
+            name: 'twitter:card',
+            content: 'summary',
+          },
+          {
+            name: 'twitter:creator',
+            content: author,
+          },
+          {
+            name: 'twitter:title',
+            content: title,
+          },
+          {
+            name: 'twitter:description',
+            content: metaDescription,
+          },
+        ].concat(meta)
+      }
+    />
+  );
+};
