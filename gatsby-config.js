d9af2d8c61d97a56a6bfa43c16eea0be401f2bad
ENvironmentSet <sbshw1@gmail.com>
2019-07-25 20:13:17 +0900

    Replace default favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         background_color: '#ffffff',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'content/assets/gatsby-icon.png' // TODO: Replace this icon
+        icon: 'content/assets/overcurried.png'
       },
     },
     'gatsby-plugin-offline',
