e39fffa1b33e6d204f79453d004c8e81b97652ac
ENvironmentSet <sbshw1@gmail.com>
2019-07-25 20:24:14 +0900

    Fix component Markdown
    Fixed:
    
    - wrong import statement for `hyperLinkColor`
    - wrong styling with `hyperLinkColor`

--- src/components/atoms/Markdown.js
+++ src/components/atoms/Markdown.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import { hyperLink as hyperLinkColor } from 'constants/palette';
+import { hyperLinkColor } from 'constants/palette';
 
 export default function Markdown({ children }) {
   const MarkdownContainer = styled.div`
       & a {
-        ${hyperLinkColor}
+        color: ${hyperLinkColor};
       }
   `;
 
