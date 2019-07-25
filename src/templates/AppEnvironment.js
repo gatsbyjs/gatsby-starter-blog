39a86826dd19e50e86016d4fbd5957a7a558e9ef
ENvironmentSet <sbshw1@gmail.com>
2019-07-25 22:15:35 +0900

    Style hr element
    changed:
    
    - hr now has same color as text content

--- src/templates/AppEnvironment.js
+++ src/templates/AppEnvironment.js
@@ -15,6 +15,9 @@ export default function AppEnvironment({ children }) {
             p, span, small, h1, h2, h3, h4, h5, h6 {
               color: ${textColor};
             }
+            hr {
+              background-color: ${textColor};
+            }
           `
         }
       />
