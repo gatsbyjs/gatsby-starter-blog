433d3fd804d4fc2e74f226434d78de334c36efee
arutuja <37767254+arutuja@users.noreply.github.com>
2019-09-15 10:46:20 +0530

    gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `My first Gatsby blog`,
+    author: `Rutuja A.`,
+    description: `Let us see what we can do`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ruutweets`,
     },
   },
   plugins: [
@@ -63,7 +63,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/gatsby-big.png`,
       },
     },
     `gatsby-plugin-offline`,
