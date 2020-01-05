41528ebe91ee088a0f5ce9a5530dc09f8149d3c8
jessecha <jessecha222@gmail.com>
2020-01-05 14:47:39 +0900

    ㅇㅇ

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `jesse's gatsby`,
     author: `jesse`,
     description: `lulu lala`,
-    siteUrl: `https://netlify.com/`,
+    siteUrl: `/`,
     social: {
       twitter: ``,
     },
