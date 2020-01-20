f98f79e53797a679ac0ae55cd8c748f5d2957aef
jessecha <jessecha222@gmail.com>
2020-01-20 22:01:46 +0900

    deleted content

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Hama Develop`,
     author: `Jesse Cha`,
-    description: `하마터면 개발 안할 뻔 했지 뭐야...`,
+    description: `좋은 일만 하고 살 수 없는 세상이지만 그렇다고 안 좋은 일만 하고 살 수는 없다.`,
     //URL이 URL의 형태를 가지지 않으면 config.js 벨리데이션에 실패한다. 반드시 url 처럼 생긴 주소를 써야 한다.
     siteUrl: `https://hamadevelop.me`,
     image:"/content/assets/gatsby-icon.png",
