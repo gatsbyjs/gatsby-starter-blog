506262f7195428b6d1636bacb7f0603854e0deb7
jessecha <jessecha222@gmail.com>
2020-01-20 21:30:14 +0900

    added meta img

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@ module.exports = {
     author: `Jesse Cha`,
     description: `하마터면 개발 안할 뻔 했지 뭐야...`,
     //URL이 URL의 형태를 가지지 않으면 config.js 벨리데이션에 실패한다. 반드시 url 처럼 생긴 주소를 써야 한다.
-    siteUrl: `https://abc.com/`,
+    siteUrl: `https://hamadevelop.me`,
+    image:"/content/assets/gatsby-icon.png",
     social: {
       twitter: ``,
     },
