d99ec661a19ef9b505082192178f470fc9376718
jesse cha <jeses.cha@navercorp.com>
2021-07-18 15:04:35 +0900

    nav 이동 에러 수정

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,83 +1,80 @@
 import React, { useEffect, useRef, useState } from "react"
 import { rhythm } from "../utils/typography"
-import { Link } from 'gatsby'
-import './nav.css';
+import { Link } from "gatsby"
+import "./nav.css"
 
-       
+const Nav = ({ tag, location }) => {
+  const [move, setMove] = useState(false)
+  let current
+  const navRef = useRef(null)
 
-const Nav = ({tag, location}) => {
-    const [move, setMove] = useState(false);
-    let current
-    const navRef = useRef(null)
+  const scrollToNav = ref => {
+    ref &&
+      ref.current &&
+      window.scrollTo({
+        top: ref.current.offsetTop,
+        left: 0,
+        behavior: "smooth",
+      })
+  }
 
-    const scrollToNav = (ref) => {
-        ref && ref.current && window.scrollTo({
-                                top: ref.current.offsetTop,
-                                left: 0,
-                                behavior: 'smooth'
-                            })
-        }
+  useEffect(() => {
+    current = document.getElementById(tag)
+    const changeFirstCategory = () => {
+      document.getElementById("all").innerText = tag
+      document
+        .getElementById("all")
+        .parentElement.classList.add("navBox-active")
+    }
+    const changeFirstCategoryToAll = () => {
+      document
+        .getElementById("all")
+        .parentElement.classList.add("navBox-active")
+    }
+    current
+      ? current.parentElement.classList.add("navBox-active")
+      : location && location.pathname === "/"
+      ? changeFirstCategoryToAll()
+      : changeFirstCategory()
 
-    useEffect(()=>{
-        
-        current = document.getElementById(tag) 
-        const changeFirstCategory  = ()=>{
-            document.getElementById('all').innerText = tag
-            document.getElementById('all').parentElement.classList.add('navBox-active')
-        }
-        const changeFirstCategoryToAll = () =>{
-            document.getElementById('all').parentElement.classList.add('navBox-active')
-        }
-        current?
-        current.parentElement.classList.add('navBox-active') : 
-        location && location.pathname === "/" ?
-        changeFirstCategoryToAll()
-        :
-        changeFirstCategory()
-
-        const navWrapper = document.querySelector('.navWrapper')
-        const navPosition = navWrapper.offsetTop
-
-    },[]
-    )
-    
+    const navWrapper = document.querySelector(".navWrapper")
+    const navPosition = navWrapper.offsetTop
+  }, [])
 
   return (
-    <div style={{
-        display: 'flex',
-        justifyContent: 'flex-start',
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
         marginBottom: rhythm(1),
-    }}>
-<div className='navWrapper' ref={navRef}>
-    <div className="navBox" >
-        <Link id={"all"} to={'/'} >
-                all
-        </Link>
-    </div>
+      }}
+    >
+      <div className="navWrapper" ref={navRef}>
+        <div className="navBox">
+          <Link id={"all"} to={"/"}>
+            all
+          </Link>
+        </div>
 
-    <div className="navBox" >
-         <Link id={'algorithm'} to={'tags/algorithm'}>
-                algorithm
-                </Link>
-        
-    </div>
-    <div className="navBox">
-        <Link id={'knowledge'} to={'tags/knowledge'}>knowledge</Link>
-    </div>
+        <div className="navBox">
+          <Link id={"algorithm"} to={"/tags/algorithm"}>
+            algorithm
+          </Link>
+        </div>
+        <div className="navBox">
+          <Link id={"knowledge"} to={"/tags/knowledge"}>
+            knowledge
+          </Link>
+        </div>
 
-    <div className="navBox" style={{
-        
-    }}>
-        <Link id={'studylog'} to={'tags/studylog'}>studylog</Link>
+        <div className="navBox" style={{}}>
+          <Link id={"studylog"} to={"/tags/studylog"}>
+            studylog
+          </Link>
+        </div>
+        <div className="navBox navBox-end">1</div>
+      </div>
     </div>
-    <div className="navBox navBox-end">
-         1
-    </div>
-    
-</div>
-
-    </div>
-    
   )
 }
 
