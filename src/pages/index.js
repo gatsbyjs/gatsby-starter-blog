7732082a752feff4aa5573ffc793942db0fc7df1
Tiago Madeira <tmadeira@gmail.com>
2021-11-05 12:24:50 -0300

    Remove unused Gatsby starter code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-import Bio from "../components/bio"
-import Layout from "../components/layout"
 import Seo from "../components/seo"
 import {
   PlasmicComponent,
@@ -12,20 +10,6 @@ import { initPlasmicLoaderWithRegistrations } from "../plasmic-init"
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
-
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
 
   const { plasmicComponents, plasmicOptions } = data
   return (
