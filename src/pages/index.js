f4dfa980e0eeb3d6cff7f534bd3ee18da41ca6fd
heriswn <heri15002@mail.unpad.ac.id>
2021-11-13 10:44:25 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const BlogIndex = ({ data, location }) => {
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
+        <Seo title="Home" />
         <Bio />
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
